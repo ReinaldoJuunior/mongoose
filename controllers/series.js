50b568a51dc1bf74b7572ff9fdb3f004e9682012
ReinaldoJuunior <gamers.jrxiii@gmail.com>
2023-04-19 22:25:36 -0300

    Edit, delete and short list

--- controllers/series.js
+++ controllers/series.js
@@ -26,6 +26,48 @@ const novaForm = (req, res) => {
     res.render('series/nova')
 }
 
+const excluir = async ({ Serie }, req, res) => {
+    try{
+        const remove = await Serie.deleteOne({
+            _id: req.params.id
+        })
+        res.redirect('/series')
+        return remove
+    }catch(err){
+        res.send(err)
+    }
+}
+
+const editarProcess = async ({Serie}, req, res) => {
+    try{
+        const serie = await Serie.findOneAndUpdate({__id: req.params.id}, (err, serie) => {
+            serie.name = req.body.name
+            serie.status = req.body.status
+            serie.save()
+        })
+        console.log('edited 2 ')
+        res.redirect('/series')
+        return serie
+    }catch(err){
+        res.render(err)
+    }
+}
+
+const editarForm = async ({Serie}, req, res) => {
+    try{
+        const labels =[
+            {id: 'to-watch', name: 'Para Assistir'},
+            {id: 'watching', name: 'Assistindo'},
+            {id: 'watched', name: 'Assistido'}
+        ]
+        const serie = await Serie.findOne({ _id: req.params.id })
+        res.render('series/editar', { serie, labels })
+        return serie
+    }catch(err){
+        res.send(err)
+    }
+}
+
 module.exports = {
-    index, novaProcess, novaForm
+    index, novaProcess, novaForm, excluir, editarForm, editarProcess
 }
